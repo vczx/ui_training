a997bd2ccda5eb2774d05e41ff9db266d773667e
Vince <vince@dev.com>
2016-11-21 21:36:14 +0800

    changes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ var path = require('path');
 
 var app = express();
 
+//hot module reloading function for webpack
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
+app.use(webpackHotMiddleware(compiler));
+//end of hot module reloading
+
 app.use(express.static('./dist'));
 
 app.use('/', function (req, res) {
